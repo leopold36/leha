33576ccf1a423a88a5bbba6e206401fc2bbcca8f
LeopoldHaex <leopoldhaex@gmail.com>
2018-05-22 13:50:42 +0000

    first database migration

--- first/models.py
+++ first/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    enity_name = models.CharField(max_length=64)
+    entity_country = models.CharField(max_length=64)
+    
