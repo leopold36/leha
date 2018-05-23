8ce7acd6c58f6ff604d4d078944ede196644fe9a
LeopoldHaex <leopoldhaex@gmail.com>
2018-05-23 13:45:58 +0000

    Entity table creation

--- first/models.py
+++ first/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Flight(models.Model):
-    enity_name = models.CharField(max_length=64)
+class Entity(models.Model):
+    entity_name = models.CharField(max_length=64)
     entity_country = models.CharField(max_length=64)
     
+    def __str__(self):
+        return self.entity_name
