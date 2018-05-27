916efd0d72d4f937fccedb02656ffe9cd4a76f73
LeopoldHaex <leopoldhaex@gmail.com>
2018-05-27 15:32:50 +0000

    relating group table

--- first/models.py
+++ first/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    code = models.CharField(max_length=3)
+    group_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.group_name
+
 class Entity(models.Model):
     entity_name = models.CharField(max_length=64)
-    entity_country = models.CharField(max_length=64)
+    entity_group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="origin")
     
     def __str__(self):
         return self.entity_name
+        
+        
+      
