bc91e911103a8295327cc95744a3fc06637c2571
LeopoldHaex <leopoldhaex@gmail.com>
2018-05-31 14:23:29 +0000

    first javascript

--- first/views.py
+++ first/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Entity, Group
+from .models import Entity
 
 # Create your views here.
 def index(request):
     context = {
-        "Group": Group.objects.all()
+        "entity": Entity.objects.all()
     }
         
     return render(request, "entity/index.html", context)
